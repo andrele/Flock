6083f9aaac19962157ba98359ce05d890dfff8b0
Chrys Francisco <chrysoberyl@172-30-28-213.dynapool.nyu.edu>
2013-09-28 23:43:29 -0400

    Refactored location view

--- js/main.js
+++ js/main.js
@@ -35,11 +35,57 @@ app.View.initialize = function(){
 			};
 			app.addUserToPeople(name, meta);
 			console.log(meta);
+			app.View.getLocation();
 	} );
+
 };
 
+app.View.drawLocation = function() {
+	template = _.template( $('#enumerateLocations').html());
+	$('#main_container').html(template());
+}
 
+app.View.getLocation = function() {
+	//var x=document.getElementById("demo");
+	var foursquareData;
 
+	function getLocation()
+		{
+		if (navigator.geolocation)
+			{
+			navigator.geolocation.getCurrentPosition(showPosition);
+			}
+		//else{ x.innerHTML="Geolocation is not supported by this browser."; }
+		}
+	function showPosition(position)
+		{
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", "https://api.foursquare.com/v2/venues/search?ll=" + position.coords.latitude + "," + position.coords.longitude + "&oauth_token=2ZBTC4SWH5UO1UTOPCXOARGZ5RXLFM3NFRVE1UNFDMNGLGPN&v=20130928", false);
+			xhr.send();
+
+			foursquareData = JSON.parse(xhr.responseText);
+
+		//x.innerHTML= "Status: " + xhr.status + "<br/>" + "StatusText: " + xhr.responseText;
+
+			var ul = $('ul#locations');
+
+			for (var i = 0;i<foursquareData.response.venues.length;i++) {
+				var listItem = document.createElement("li");
+				listItem.setAttribute("data-id", foursquareData.response.venues[i].id );
+				listItem.setAttribute("onclick", "locationClicked(this)");
+				listItem.appendChild(document.createTextNode(foursquareData.response.venues[i].name));
+				ul.appendChild(listItem); 
+			}
+
+			//x.appendChild(ul);
+
+		}
+
+		function locationClicked(element) {
+			console.log("Name: " + element.innerHTML);
+			console.log("Id: " + element.getAttribute("data-id"));
+		}
+}
 
 
 });
